49f581b1475dce27a9b85638f7b9cb93c17392c0
moshe.nas <moshen733@gmail.com>
2024-06-18 21:07:26 +0300

    added class vector quantizer

---
+++ vector_quantization.py
@@ -0,0 +1,11 @@
+import torch
+from torch import nn
+import numpy as np
+
+
+class VectorQuantizer(nn.Module):
+    def __init__(self):
+        super(VectorQuantizer, self).__init__()
+
+    def forward(self, x):
+        pass
