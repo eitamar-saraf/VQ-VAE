3e14ce9818256ae79c3ac9a910a043008916853b
moshe.nas <moshen733@gmail.com>
2024-06-18 22:04:50 +0300

    fixed shape bug and added running code

--- vector_quantization.py
+++ vector_quantization.py
@@ -6,7 +6,7 @@ import numpy as np
 class VectorQuantizer(nn.Module):
     def __init__(self, embeddings: torch.Tensor):
         super(VectorQuantizer, self).__init__()
-        self.embedding_dim = embeddings.shape()
+        self.embedding_dim = embeddings.shape
         self.embeddings = embeddings.reshape((self.embedding_dim[0], -1))
 
     def forward(self, x: torch.Tensor):
@@ -14,11 +14,22 @@ class VectorQuantizer(nn.Module):
             Assuming x: (B,C[,H[,W]])
             embeddings: (K, D) - D=C/CH/CHW
         """
-        batch_size = x.shape()[0]
+        batch_size = x.shape[0]
         x = x.reshape((batch_size, -1))
         K, D = self.embedding_dim
         nearest_embedding = np.zeros((B, D))
         for i in range(batch_size):
-            distance_vector = torch.linalg.vector_norm(self.embeddings - x[i])
+            distance_vector = torch.linalg.vector_norm(self.embeddings - x[i], dim=1)
             nearest_embedding[i] = self.embeddings[torch.argmin(distance_vector)]
         return nearest_embedding
+
+
+
+
+if __name__ == '__main__':
+    embeddings = torch.eye(10, 3*10*10)
+    B, C, H, W = 4, 3, 10, 10
+    x = torch.randn((B, C, H, W))
+    vq = VectorQuantizer(embeddings=embeddings)
+    print(x)
+    print(vq(x))
